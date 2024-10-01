b9728ab0ba49aa292d84b7fa8924537ccfcb0956
Francisco Villar <franciscovillar1108@gmail.com>
2024-10-01 17:38:56 -0300

    Agregar archivos de proyecto.

---
+++ EVA2_FranciscoVillar/Form1.Designer.vb
@@ -0,0 +1,253 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        LblRut = New Label()
+        LblNombres = New Label()
+        LblApellidos = New Label()
+        LblSexo = New Label()
+        RadioBtnMasculino = New RadioButton()
+        RadioBtnFemenino = New RadioButton()
+        RadioBtnIndefinido = New RadioButton()
+        ComboBoxComunas = New ComboBox()
+        LblComuna = New Label()
+        LblCiudad = New Label()
+        Label1 = New Label()
+        TxtRut = New TextBox()
+        TxtNombres = New TextBox()
+        TxtApellidos = New TextBox()
+        TxtCiudad = New TextBox()
+        TxtObservacion = New TextBox()
+        BtnBuscar = New Button()
+        BtnGuardar = New Button()
+        SuspendLayout()
+        ' 
+        ' LblRut
+        ' 
+        LblRut.AutoSize = True
+        LblRut.Location = New Point(22, 46)
+        LblRut.Name = "LblRut"
+        LblRut.Size = New Size(39, 20)
+        LblRut.TabIndex = 0
+        LblRut.Text = "RUT:"
+        ' 
+        ' LblNombres
+        ' 
+        LblNombres.AutoSize = True
+        LblNombres.Location = New Point(22, 91)
+        LblNombres.Name = "LblNombres"
+        LblNombres.Size = New Size(73, 20)
+        LblNombres.TabIndex = 1
+        LblNombres.Text = "Nombres:"
+        ' 
+        ' LblApellidos
+        ' 
+        LblApellidos.AutoSize = True
+        LblApellidos.Location = New Point(22, 133)
+        LblApellidos.Name = "LblApellidos"
+        LblApellidos.Size = New Size(75, 20)
+        LblApellidos.TabIndex = 2
+        LblApellidos.Text = "Apellidos:"
+        ' 
+        ' LblSexo
+        ' 
+        LblSexo.AutoSize = True
+        LblSexo.Location = New Point(22, 181)
+        LblSexo.Name = "LblSexo"
+        LblSexo.Size = New Size(426, 20)
+        LblSexo.TabIndex = 3
+        LblSexo.Text = "Sexo: ---------------------------------------------------------------"
+        ' 
+        ' RadioBtnMasculino
+        ' 
+        RadioBtnMasculino.AutoSize = True
+        RadioBtnMasculino.Location = New Point(30, 220)
+        RadioBtnMasculino.Name = "RadioBtnMasculino"
+        RadioBtnMasculino.Size = New Size(94, 24)
+        RadioBtnMasculino.TabIndex = 4
+        RadioBtnMasculino.TabStop = True
+        RadioBtnMasculino.Text = "Masculino"
+        RadioBtnMasculino.UseVisualStyleBackColor = True
+        ' 
+        ' RadioBtnFemenino
+        ' 
+        RadioBtnFemenino.AutoSize = True
+        RadioBtnFemenino.Location = New Point(182, 220)
+        RadioBtnFemenino.Name = "RadioBtnFemenino"
+        RadioBtnFemenino.Size = New Size(92, 24)
+        RadioBtnFemenino.TabIndex = 5
+        RadioBtnFemenino.TabStop = True
+        RadioBtnFemenino.Text = "Femenino"
+        RadioBtnFemenino.UseVisualStyleBackColor = True
+        ' 
+        ' RadioBtnIndefinido
+        ' 
+        RadioBtnIndefinido.AutoSize = True
+        RadioBtnIndefinido.Location = New Point(352, 220)
+        RadioBtnIndefinido.Name = "RadioBtnIndefinido"
+        RadioBtnIndefinido.Size = New Size(95, 24)
+        RadioBtnIndefinido.TabIndex = 6
+        RadioBtnIndefinido.TabStop = True
+        RadioBtnIndefinido.Text = "Indefinido"
+        RadioBtnIndefinido.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBoxComunas
+        ' 
+        ComboBoxComunas.FormattingEnabled = True
+        ComboBoxComunas.Location = New Point(122, 284)
+        ComboBoxComunas.Name = "ComboBoxComunas"
+        ComboBoxComunas.Size = New Size(325, 27)
+        ComboBoxComunas.TabIndex = 7
+        ComboBoxComunas.Text = "(Seleccione una comuna)"
+        ' 
+        ' LblComuna
+        ' 
+        LblComuna.AutoSize = True
+        LblComuna.Location = New Point(22, 287)
+        LblComuna.Name = "LblComuna"
+        LblComuna.Size = New Size(67, 20)
+        LblComuna.TabIndex = 8
+        LblComuna.Text = "Comuna:"
+        ' 
+        ' LblCiudad
+        ' 
+        LblCiudad.AutoSize = True
+        LblCiudad.Location = New Point(22, 363)
+        LblCiudad.Name = "LblCiudad"
+        LblCiudad.Size = New Size(59, 20)
+        LblCiudad.TabIndex = 9
+        LblCiudad.Text = "Ciudad:"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(22, 434)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(94, 20)
+        Label1.TabIndex = 10
+        Label1.Text = "Observacion:"
+        ' 
+        ' TxtRut
+        ' 
+        TxtRut.Location = New Point(67, 40)
+        TxtRut.Name = "TxtRut"
+        TxtRut.Size = New Size(232, 26)
+        TxtRut.TabIndex = 11
+        ' 
+        ' TxtNombres
+        ' 
+        TxtNombres.Location = New Point(122, 85)
+        TxtNombres.Name = "TxtNombres"
+        TxtNombres.Size = New Size(325, 26)
+        TxtNombres.TabIndex = 12
+        ' 
+        ' TxtApellidos
+        ' 
+        TxtApellidos.Location = New Point(122, 127)
+        TxtApellidos.Name = "TxtApellidos"
+        TxtApellidos.Size = New Size(325, 26)
+        TxtApellidos.TabIndex = 13
+        ' 
+        ' TxtCiudad
+        ' 
+        TxtCiudad.Location = New Point(122, 357)
+        TxtCiudad.Name = "TxtCiudad"
+        TxtCiudad.Size = New Size(325, 26)
+        TxtCiudad.TabIndex = 14
+        ' 
+        ' TxtObservacion
+        ' 
+        TxtObservacion.Location = New Point(122, 431)
+        TxtObservacion.Name = "TxtObservacion"
+        TxtObservacion.Size = New Size(325, 26)
+        TxtObservacion.TabIndex = 15
+        ' 
+        ' BtnBuscar
+        ' 
+        BtnBuscar.BackColor = SystemColors.ScrollBar
+        BtnBuscar.Location = New Point(319, 32)
+        BtnBuscar.Name = "BtnBuscar"
+        BtnBuscar.Size = New Size(128, 41)
+        BtnBuscar.TabIndex = 16
+        BtnBuscar.Text = "Buscar"
+        BtnBuscar.UseVisualStyleBackColor = False
+        ' 
+        ' BtnGuardar
+        ' 
+        BtnGuardar.BackColor = SystemColors.ScrollBar
+        BtnGuardar.Location = New Point(122, 518)
+        BtnGuardar.Name = "BtnGuardar"
+        BtnGuardar.Size = New Size(221, 48)
+        BtnGuardar.TabIndex = 17
+        BtnGuardar.Text = "Guardar MySql"
+        BtnGuardar.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 19F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(490, 617)
+        Controls.Add(BtnGuardar)
+        Controls.Add(BtnBuscar)
+        Controls.Add(TxtObservacion)
+        Controls.Add(TxtCiudad)
+        Controls.Add(TxtApellidos)
+        Controls.Add(TxtNombres)
+        Controls.Add(TxtRut)
+        Controls.Add(Label1)
+        Controls.Add(LblCiudad)
+        Controls.Add(LblComuna)
+        Controls.Add(ComboBoxComunas)
+        Controls.Add(RadioBtnIndefinido)
+        Controls.Add(RadioBtnFemenino)
+        Controls.Add(RadioBtnMasculino)
+        Controls.Add(LblSexo)
+        Controls.Add(LblApellidos)
+        Controls.Add(LblNombres)
+        Controls.Add(LblRut)
+        Name = "Form1"
+        Text = "EVA2_FranciscoVillar"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LblRut As Label
+    Friend WithEvents LblNombres As Label
+    Friend WithEvents LblApellidos As Label
+    Friend WithEvents LblSexo As Label
+    Friend WithEvents RadioBtnMasculino As RadioButton
+    Friend WithEvents RadioBtnFemenino As RadioButton
+    Friend WithEvents RadioBtnIndefinido As RadioButton
+    Friend WithEvents ComboBoxComunas As ComboBox
+    Friend WithEvents LblComuna As Label
+    Friend WithEvents LblCiudad As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TxtRut As TextBox
+    Friend WithEvents TxtNombres As TextBox
+    Friend WithEvents TxtApellidos As TextBox
+    Friend WithEvents TxtCiudad As TextBox
+    Friend WithEvents TxtObservacion As TextBox
+    Friend WithEvents BtnBuscar As Button
+    Friend WithEvents BtnGuardar As Button
+
+End Class
---
+++ EVA2_FranciscoVillar/Form1.vb
@@ -0,0 +1,181 @@
+﻿Imports MySql.Data.MySqlClient
+
+
+Public Class Form1
+    'Conexión MySQL
+    Dim connectionString As String = "Server=localhost;Database=registropersonas;User ID='root';Password='';"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Llenar el ComboBox con comunas desde la base de datos
+        CargarComunAs()
+    End Sub
+
+    Private Sub CargarComunAs()
+        Using conn As New MySqlConnection(connectionString)
+            Try
+                conn.Open()
+                Dim sql As String = "SELECT NombreComuna FROM comuna"
+
+                Using cmd As New MySqlCommand(sql, conn)
+                    Using reader As MySqlDataReader = cmd.ExecuteReader()
+                        ComboBoxComunas.Items.Clear() ' Limpia items existentes
+                        While reader.Read()
+                            ComboBoxComunas.Items.Add(reader("NombreComuna").ToString())
+                        End While
+                    End Using
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Error al cargar las comunas: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Dim rut As String = txtRut.Text
+        Dim nombre As String = txtNombres.Text
+        Dim apellido As String = txtApellidos.Text
+        Dim sexo As String
+
+
+        If RadioBtnMasculino.Checked Then
+            sexo = "Masculino"
+        ElseIf RadioBtnFemenino.Checked Then
+            sexo = "Femenino"
+        ElseIf RadioBtnIndefinido.Checked Then
+            sexo = "Indefinido"
+        Else
+            MessageBox.Show("Por favor, seleccione el sexo.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Obtener otros campos
+        Dim comuna As String = ComboBoxComunas.SelectedItem?.ToString()
+        Dim ciudad As String = TxtCiudad.Text
+        Dim observacion As String = TxtObservacion.Text
+
+        ' Validar campos obligatorios
+        If String.IsNullOrWhiteSpace(rut) Or String.IsNullOrWhiteSpace(nombre) Or String.IsNullOrWhiteSpace(apellido) Or String.IsNullOrWhiteSpace(comuna) Then
+            MessageBox.Show("Por favor, complete todos los campos obligatorios (RUT, Nombre, Apellido y Comuna).", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Guardar los datos en la base de datos
+        Using conn As New MySqlConnection(connectionString)
+            Try
+                conn.Open()
+
+                Dim sql As String = "INSERT INTO personas (RUT, Nombre, Apellido, Sexo, Comuna, Ciudad, Observacion) " &
+                                "VALUES (@rut, @nombre, @apellido, @sexo, @comuna, @ciudad, @observacion)"
+
+                Using cmd As New MySqlCommand(sql, conn)
+                    cmd.Parameters.AddWithValue("@rut", rut)
+                    cmd.Parameters.AddWithValue("@nombre", nombre)
+                    cmd.Parameters.AddWithValue("@apellido", apellido)
+                    cmd.Parameters.AddWithValue("@sexo", sexo)
+                    cmd.Parameters.AddWithValue("@comuna", comuna)
+                    cmd.Parameters.AddWithValue("@ciudad", ciudad)
+                    cmd.Parameters.AddWithValue("@observacion", observacion)
+
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Datos guardados exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                    ' Limpia el formulario después de guardar
+                    LimpiarFormulario()
+                End Using
+
+            Catch ex As Exception
+                MessageBox.Show("Error al guardar los datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End Using
+    End Sub
+
+    ' Método para limpiar el formulario
+    Private Sub LimpiarFormulario()
+        TxtRut.Clear()
+        TxtNombres.Clear()
+        TxtApellidos.Clear()
+        TxtCiudad.Clear()
+        TxtObservacion.Clear()
+        RadioBtnMasculino.Checked = False
+        RadioBtnFemenino.Checked = False
+        RadioBtnIndefinido.Checked = False
+        ComboBoxComunas.SelectedIndex = -1
+        TxtRut.Focus() ' Coloca el cursor en el campo "RUT"
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim rut As String = txtRut.Text
+
+        ' Validar que el campo RUT no esté vacío
+        If String.IsNullOrWhiteSpace(rut) Then
+            MessageBox.Show("Por favor, ingrese el RUT.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Buscar el registro en la base de datos por RUT
+        Using conn As New MySqlConnection(connectionString)
+            Try
+                conn.Open()
+
+                Dim sql As String = "SELECT * FROM personas WHERE RUT = @rut"
+
+                Using cmd As New MySqlCommand(sql, conn)
+                    cmd.Parameters.AddWithValue("@rut", rut)
+
+                    Using reader As MySqlDataReader = cmd.ExecuteReader()
+                        If reader.Read() Then
+                            ' Si se encuentra el registro, rellenar los campos
+                            txtNombres.Text = reader("Nombre").ToString()
+                            txtApellidos.Text = reader("Apellido").ToString()
+                            Dim sexo As String = reader("Sexo").ToString()
+
+                            ' Seleccionar el radio button según el valor del sexo
+                            Select Case sexo
+                                Case "Masculino"
+                                    RadioBtnMasculino.Checked = True
+                                Case "Femenino"
+                                    RadioBtnFemenino.Checked = True
+                                Case "Indefinido"
+                                    RadioBtnIndefinido.Checked = True
+                            End Select
+
+                            ComboBoxComunas.SelectedItem = reader("Comuna").ToString()
+                            TxtCiudad.Text = reader("Ciudad").ToString()
+                            txtObservacion.Text = reader("Observacion").ToString()
+
+                            MessageBox.Show("Registro encontrado.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        Else
+                            MessageBox.Show("No se encontró el registro con el RUT ingresado.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                            ' Limpiar el formulario si no se encuentra el registro
+                            LimpiarFormulario()
+
+                            ' Habilita todos los campos exepto el de búsqueda
+                            TxtNombres.Enabled = True
+                            TxtApellidos.Enabled = True
+                            RadioBtnMasculino.Enabled = True
+                            RadioBtnFemenino.Enabled = True
+                            RadioBtnIndefinido.Enabled = True
+                            ComboBoxComunas.Enabled = True
+                            TxtCiudad.Enabled = True
+                            TxtObservacion.Enabled = True
+
+                            ' Habilita botón Guardar
+                            BtnGuardar.Enabled = True
+
+                            ' Deshabilita el botón de búsquedaa
+                            BtnBuscar.Enabled = True
+                        End If
+                    End Using
+                End Using
+
+            Catch ex As Exception
+                MessageBox.Show("Error al buscar el registro: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub LblRut_Click(sender As Object, e As EventArgs) Handles LblRut.Click
+
+    End Sub
+End Class
